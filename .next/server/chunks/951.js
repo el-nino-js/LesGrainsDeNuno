5cc95e01f474a456a9953a8de02ebfc4556ffc77
el-nino-js <nuno.silvapinto@outlook.com>
2022-12-09 01:18:04 -0500

    Optimization

---
+++ .next/server/chunks/951.js
@@ -0,0 +1,74 @@
+"use strict";
+exports.id = 951;
+exports.ids = [951];
+exports.modules = {
+
+/***/ 951:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var framer_motion__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9034);
+/* harmony import */ var framer_motion__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(framer_motion__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(968);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _grid_item__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(8598);
+
+
+
+
+const variants = {
+    hidden: {
+        opacity: 0,
+        x: 0,
+        y: 20
+    },
+    enter: {
+        opacity: 1,
+        x: 0,
+        y: 0
+    },
+    exit: {
+        opacity: 0,
+        x: 0,
+        y: 20
+    }
+};
+const Layout = ({ children , title  })=>/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.article, {
+        initial: "hidden",
+        animate: "enter",
+        exit: "exit",
+        variants: variants,
+        transition: {
+            duration: 0.4,
+            type: "easeInOut"
+        },
+        style: {
+            position: "relative"
+        },
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+            children: [
+                title && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_head__WEBPACK_IMPORTED_MODULE_2___default()), {
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("title", {
+                        children: [
+                            title,
+                            " - Nuno Silva Pinto"
+                        ]
+                    })
+                }),
+                children,
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_grid_item__WEBPACK_IMPORTED_MODULE_3__/* .GridItemStyle */ .Pg, {})
+            ]
+        })
+    })
+;
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Layout);
+
+
+/***/ })
+
+};
+;
