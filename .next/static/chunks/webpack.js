5da78f780744f0b19438b7f068c43171dcd068ae
el-nino-js <nuno.silvapinto@outlook.com>
2024-02-06 10:56:22 -0500

    remove weatermark

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9aaba5ebaeeb4d7b"; }
+/******/ 		__webpack_require__.h = function() { return "b112f253dcc29ece"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
