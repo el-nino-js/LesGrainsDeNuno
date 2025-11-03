7951200164c65885b480732050563c50aeffb54a
el-nino-js <nuno.silvapinto@outlook.com>
2025-11-03 02:10:51 -0500

    header

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Navbar from '../navbar'
 import NoSsr from '../no-ssr'
 import { Box, Container } from '@chakra-ui/react'
-import VoxelDog from '../voxel-dog'
+
 
 
 const Main = ({children, router}) => {
     return (
         <Box as="main" pb={8}>
             <Head>
-            <meta charset="utf-8" />
-	        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta charSet="utf-8" />
+	        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <title>Poussin des forêts - Page Principale</title>
             </Head>
@@ -19,7 +19,7 @@ const Main = ({children, router}) => {
 
             <Container maxW="container.md" pt={14}>
                 <NoSsr>
-                    <VoxelDog />
+                    
                 </NoSsr>
                 {children}
             </Container>
